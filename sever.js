c2a6658370b67f6eeb3440262f4618fdb078cb23
zhaoshuhong <2459636918@qq.com>
2019-07-30 17:18:56 +0800

    add

--- sever.js
+++ sever.js
@@ -1,10 +1,22 @@
 //experss
 let experss = require('express');
-
 let app = experss();
+
+
+//启用服务的端口
+let webpack = require('webpack');
+let middle = require('webpack-dev-middleware');
+let config = require('./webpack.config');
+let compiler = webpack(compiler);
+app.use(middle(compiler));
+
+//-----
+
+
 
 app.get('/api/user',(req,res)=>{
     res.json({name:'ceshi'})
 })
 console.log('kaiqi')
-app.listen(3000)
+app.listen(3000);
+
